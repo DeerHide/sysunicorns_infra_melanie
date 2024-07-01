601b739eff03e559f31413725147aed525d32125
miragecentury <victorien.vanroye@gmail.com>
2024-07-01 17:10:43 +0200

    feat: :sparkles: add/update scripts to use ansible-vault

--- scripts/apply_ansible_on_prod.sh
+++ scripts/apply_ansible_on_prod.sh
@@ -2,7 +2,27 @@
 
 ANSIBLE_INVENTORY=ansible/inventories/inventory.ini
 
+# Colors
+RED='\033[0;31m'
+ORANGE='\033[0;33m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+# Load environment variables from .env file if it exists
+if [[ -f .env ]]; then
+  export $(cat .env | xargs)
+fi
+
+# Check if the ANSIBLE_VAULT_PASSWORD_FILE environment variable is set
+if [[ -z "${ANSIBLE_VAULT_PASSWORD_FILE}" ]]
+then
+  echo "$RED ANSIBLE_VAULT_PASSWORD_FILE is not set $NC"
+  exit 1
+fi
+
 # Launch ansible playbook
 ansible-playbook \
     -i ${ANSIBLE_INVENTORY} \
+    --vault-id=default@ansible-vars.yml \
+    --vault-password-file="${ANSIBLE_VAULT_PASSWORD_FILE}" \
     ./ansible/playbook.yml
