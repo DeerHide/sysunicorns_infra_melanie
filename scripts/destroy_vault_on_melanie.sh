15dc8e0b4cc38fc23ea14eed71a2edf5fcf5718e
miragecentury <victorien.vanroye@gmail.com>
2024-07-09 00:14:16 +0200

    feat(vault): :sparkles: add script to destroy it

---
+++ scripts/destroy_vault_on_melanie.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# set -euo pipefail
+
+function on_melanie() {
+  ssh melanie.localunicorns.info -C "$@"
+}
+
+on_melanie docker stop svc_vault
+on_melanie docker rm svc_vault
+on_melanie docker volume rm svc_vault_config svc_vault_file svc_vault_logs
+on_melanie sudo rm -rf /var/docker_volumes/svc_vault_*
