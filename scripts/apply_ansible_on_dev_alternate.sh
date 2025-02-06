5c889e38066b4995eaa4c8dc85f5617d3e46f1a6
nakool <alain.krzeczowski@gmail.com>
2025-02-06 23:16:47 +0100

    feat: 🎸 Create new script for alternate dev machine

---
+++ scripts/apply_ansible_on_dev_alternate.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# Activate python virtualenv
+if [[ -d .venv ]]; then
+  source .venv/bin/activate
+fi
+
+# Re-Use vagrant inventory file
+ANSIBLE_INVENTORY=alternate_inventory.yml
+
+# Colors
+RED='\033[0;31m'
+ORANGE='\033[0;33m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+# Load environment variables from .env file if it exists
+if [[ -f .env ]]; then
+  export $(cat .env | xargs)
+fi
+
+# Check if the ANSIBLE_VAULT_PASSWORD_FILE environment variable is set
+if [[ -z "${ANSIBLE_VAULT_PASSWORD_FILE}" ]]
+then
+  echo "$RED ANSIBLE_VAULT_PASSWORD_FILE is not set $NC"
+  exit 1
+fi
+
+
+# Launch ansible playbook
+ansible-playbook \
+    -i ${ANSIBLE_INVENTORY} \
+    --vault-id="default@${ANSIBLE_VAULT_PASSWORD_FILE}" \
+    -e @ansible-vars.yml \
+    -vv \
+    ./ansible/vagrant-playbook.yml
